83ce536e9b42dadda24404fb053d6f2e0fefbe58
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-17 16:36:19 +0530

    create sign in page
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
-  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },  { path: 'sign-in', loadChildren: './pages/sign-in/sign-in.module#SignInPageModule' }
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },  { path: 'sign-in', loadChildren: './pages/sign-in/sign-in.module#SignInPageModule' },
+  { path: 'sign-up', loadChildren: './pages/sign-up/sign-up.module#SignUpPageModule' }
 
 ];
 @NgModule({
