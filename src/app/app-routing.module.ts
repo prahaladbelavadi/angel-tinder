86c53a1728cb04eb17d5625354010d03b4938111
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-18 17:21:04 +0530

    move angel profile into pages
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
-  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },  { path: 'sign-in', loadChildren: './pages/sign-in/sign-in.module#SignInPageModule' },
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
+  { path: 'sign-in', loadChildren: './pages/sign-in/sign-in.module#SignInPageModule' },
   { path: 'sign-up', loadChildren: './pages/sign-up/sign-up.module#SignUpPageModule' },
   { path: 'startup-profile', loadChildren: './pages/startup-profile/startup-profile.module#StartupProfilePageModule' },
-  { path: 'angel-profile', loadChildren: './angel-profile/angel-profile.module#AngelProfilePageModule' }
+  { path: 'angel-profile', loadChildren: './pages/angel-profile/angel-profile.module#AngelProfilePageModule' }
 
 ];
 @NgModule({
