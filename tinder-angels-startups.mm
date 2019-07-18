1faf2ac3eb548f095b91373ddf02d2b243cbf5f1
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-18 14:43:58 +0530

    add a mind map of progression
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

---
+++ tinder-angels-startups.mm
@@ -0,0 +1,40 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1563362006286" ID="ID_727589551" MODIFIED="1563397001338" STYLE="fork" TEXT="Welcomepage">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1563395363746" HGAP="37" ID="ID_552675168" MODIFIED="1563397001895" POSITION="left" STYLE="fork" TEXT="Sign in page" VSHIFT="-87">
+<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1563395636899" ID="ID_448345811" MODIFIED="1563397002188" STYLE="fork" TEXT="tabs">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1563396637272" HGAP="52" ID="ID_1574194242" MODIFIED="1563397002432" STYLE="fork" TEXT="Denied" VSHIFT="-46">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1563396882921" ID="ID_1566180763" MODIFIED="1563397002620" STYLE="fork" TEXT="Home">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1563396925653" ID="ID_853819417" MODIFIED="1563397002819" STYLE="fork" TEXT="Set for due diligence">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1563395381083" HGAP="59" ID="ID_417288579" MODIFIED="1563397002990" POSITION="left" STYLE="fork" TEXT="Sign up page" VSHIFT="46">
+<edge COLOR="#808080" STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1563396088636" ID="ID_586031950" MODIFIED="1563397003498" STYLE="fork" TEXT="Profile">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1563396110100" ID="ID_1646027864" MODIFIED="1563397003909" STYLE="fork" TEXT="What are you looking for">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1563396400049" ID="ID_732391085" MODIFIED="1563397004192" STYLE="fork" TEXT="Angel / Startup">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
